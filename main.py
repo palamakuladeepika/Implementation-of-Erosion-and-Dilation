ff3099a494089da9d3844612319f9dbc56ba7c66
palamakuladeepika <94154679+palamakuladeepika@users.noreply.github.com>
2022-06-10 13:24:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# Import the necessary packages
+
+import cv2
+import numpy
+
+
+# Create the Text using cv2.putText
+
+NameImage = numpy.zeros((100,1000),dtype='uint8')
+font = cv2.FONT_ITALIC
+cv2.putText(NameImage,'P.Deepika',(50,70),font,2,(255),5,cv2.LINE_4)
+cv2.imshow("Name Image",NameImage)
+
+
+
+# Create the structuring element
+
+kernel1 = cv2.getStructuringElement(cv2.MORPH_CROSS,(7,7))
+
+
+
+# Erode the image
+
+erodeImage = cv2.erode(NameImage,kernel1)
+
+
+
+# Dilate the image
+
+
+dilationImage = cv2.dilate(NameImage,kernel1)
+
+# Displaying the image
+cv2.imshow("Name Image",NameImage)
+cv2.imshow("Erode Image",erodeImage)
+cv2.imshow("Dilated Image",dilationImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
